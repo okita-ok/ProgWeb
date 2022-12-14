8beeac886f200dbd4ee77701e79812ea4ca72ec3
okita-ok <109244075+okita-ok@users.noreply.github.com>
2022-12-14 17:24:16 -0400

    Commit prova1

---
+++ prova1/questao_05.js
@@ -0,0 +1,34 @@
+class Venda{
+    constructor(id, quant, preco){
+        this.id = id;
+        this.quant = quant;
+        this.preco = preco;
+    }
+    getId(){
+        return this.id;
+    }
+    setId(novoId){
+        this.id = novoId;
+    }
+    getQuant(){
+        return this.quant;
+    }
+    setQuant(novoQuant){
+        this.quant = novoQuant;
+    }
+    getPreco(){
+        return this.preco;
+    }
+    setPreco(novoPreco){
+        this.preco = novoPreco;
+    }
+    getValorTotal(){
+        return this.quant*this.preco;
+    }
+}
+
+produto1 = new Venda(123,6,40);
+produto2 = new Venda(456,2,80);
+
+console.log("Produto1: " + produto1.getValorTotal());
+console.log("Produto2: " + produto2.getValorTotal());
